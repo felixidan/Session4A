4bc6100b6e4462cbbc281cc57ddeaa562fe54c1d
felixidan <felix.idan@gmail.com>
2015-02-20 18:46:55 +0200

    Activity Lifecycle Logging Implementation

--- LifecycleLogging/app/src/main/java/com/example/felixidan/lifecyclelogging/MainActivity.java
+++ LifecycleLogging/app/src/main/java/com/example/felixidan/lifecyclelogging/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.felixidan.lifecyclelogging;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate()");
     }
 
 
@@ -35,5 +39,49 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause()");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        Log.i(TAG, "onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart()");
     }
 }
